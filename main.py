3e4601fe2a518899bcdcefc45b5211e696b25535
Igor Pavlichenko <pawigor@komcity.ru>
2015-10-05 09:17:41 +1000

    scheduling

--- main.py
+++ main.py
@@ -1,41 +1,150 @@
+# -*- coding: utf-8 -*-
+import os
+import socket
+import threading
+import time
+
+import schedule
+import telegram
+from flask import Flask, request
+
 __author__ = 'z'
 
-import telegram
+ANT_COUNT = 1
+job = None
 
 
 def get_token():
-    # global token
     f = open('bot.token', 'r')
-    token = f.readline()
-    return token
+    return f.readline()
 
 
 # return get_token()
 LAST_UPDATE_ID = None
+CERT = 'server.crt'
+CERT_KEY = 'server.key'
 
-bot = telegram.Bot(token=get_token())
+token = get_token()
+bot = telegram.Bot(token=token)
 
 print(bot.getMe())
 
-
-def echo():
-    global LAST_UPDATE_ID
-    updates = bot.getUpdates(offset=LAST_UPDATE_ID)
-    for u in updates:
-        print(u.message)
-        message_chat_id = u.message.chat_id
-        print(message_chat_id)
-        chat_id = message_chat_id
-        message = u.message.text
-        # ('utf-8')
-        print(message)
-        if message:
-            bot.sendMessage(chat_id=chat_id, text=message)
-        LAST_UPDATE_ID = u.update_id + 1
-
-        # bot.sendMessage(u.message)
-        # bot.message(u.de_json(u.message))
+print(bot.setWebhook(webhook_url="https://ngor44.dlinkddns.com./hook", certificate=open(CERT, 'rb')))
 
 
-while True:
-    echo()
+# print(bot.getUpdates()[-1].message.text)
+
+
+app = Flask(__name__)
+
+
+def other_job():
+    print("other")
+    bot.sendMessage(chat_id=58174909, text=os.popen('date').read())
+
+
+def test_job():
+    global ANT_COUNT
+    print('test')
+    bot.sendMessage(chat_id=58174909, text=telegram.Emoji.ANT * ANT_COUNT)
+
+
+def schedule_start():
+    global cease_continuous_run
+    # schedule.every(10).seconds.do(test_job)
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(1)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+
+
+schedule_start()
+
+
+@app.route("/")
+def hello():
+    print("Hello")
+    return 'hello'
+
+
+@app.route("/hook", methods=['POST'])
+def hook():
+    # print("hook")
+    if request.method == "POST":
+        global ANT_COUNT
+        global job
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
+
+        print(update.message)
+        chat_id = update.message.chat.id
+        msg_id = update.message.message_id
+
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
+        if text:
+            # print(text[0], text[1], text[2], text[3])
+            # print(text[0:4])
+            print(text)
+            if text.lower() == '/cmd':
+                custom_keyboard = [
+                    [telegram.Emoji.THUMBS_UP_SIGN, telegram.Emoji.THUMBS_DOWN_SIGN, telegram.Emoji.ALARM_CLOCK,
+                     telegram.Emoji.ALIEN_MONSTER]]
+                reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+                bot.sendMessage(chat_id=chat_id, text=socket.gethostname(), reply_to_message_id=msg_id,
+                                reply_markup=reply_markup)
+            elif text.lower() == 'status':
+                bot.sendMessage(chat_id=chat_id, text='Online. I\'m ' + bot.getMe().username)
+            elif text.lower() == "ls":
+                t = os.popen("find /home/pi/age/Подкасты/ -type f -newer /home/pi/age/Подкасты/last").read()
+                bot.sendMessage(chat_id=chat_id, text=t)
+            elif text == telegram.Emoji.ALARM_CLOCK:
+                job = schedule.every(5).seconds.do(other_job)
+                bot.sendMessage(chat_id=chat_id, text="set alarmClock")
+            elif text == telegram.Emoji.ALIEN_MONSTER:
+                for job in schedule.jobs:
+                    schedule.cancel_job(job)
+            elif text == telegram.Emoji.THUMBS_UP_SIGN:
+                ANT_COUNT += 1
+                bot.sendMessage(chat_id=chat_id, text=str(ANT_COUNT))
+            elif text == telegram.Emoji.THUMBS_DOWN_SIGN:
+                ANT_COUNT -= 1
+                bot.sendMessage(chat_id=chat_id, text=str(ANT_COUNT))
+            else:
+                # repeat the same message back (echo)
+                bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+
+
+# def echo():
+#     global LAST_UPDATE_ID
+#     updates = bot.getUpdates(offset=LAST_UPDATE_ID)
+#     for u in updates:
+#         print(u.message)
+#         message_chat_id = u.message.chat_id
+#         print(message_chat_id)
+#         chat_id = message_chat_id
+#         message = u.message.text
+#         # ('utf-8')
+#         print(message)
+#         if message:
+#             bot.sendMessage(chat_id=chat_id, text=message)
+#         LAST_UPDATE_ID = u.update_id + 1
+#
+#         # bot.sendMessage(u.message)
+#         # bot.message(u.de_json(u.message))
+#
+#
+# while True:
+#     echo()
+context = (CERT, CERT_KEY)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, ssl_context=context)
